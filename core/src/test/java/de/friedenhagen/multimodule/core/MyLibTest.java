275983018bde42b5da00ebe2c6ec747b0028e7ae
Mirko Friedenhagen <mirko-lists@friedenhagen.de>
2011-04-18 21:23:43 +0200

    Introduce a test.

---
+++ core/src/test/java/de/friedenhagen/multimodule/core/MyLibTest.java
@@ -0,0 +1,24 @@
+package de.friedenhagen.multimodule.core;
+
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+public class MyLibTest {
+
+    @Test
+    public void testTested() {
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        final PrintStream out = new PrintStream(stream);
+        try {
+            final MyLib lib = new MyLib(out);
+            lib.tested();
+        } finally {
+            out.close();
+        }
+        Assert.assertEquals("I am tested\n", stream.toString());
+    }
+}
